131d1a1ca12868e6488acfb06e865a1e1b93b82b
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2011-12-14 20:13:06 -0500

    Updated to work with Leiningen 1.6.2 and Clojure 1.2.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject radagast "1.0.0"
+(defproject radagast "1.1.0"
   :description "Simplistic test coverage."
-  :dev-dependencies [[org.clojure/clojure "1.1.0"]
-                     [org.clojure/clojure-contrib "1.1.0"]
-                     [swank-clojure "1.2.1"]])
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]])
