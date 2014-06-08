6ceba32e34193dfb6cd196722f50c810803d2bd5
arrdem <me@arrdem.com>
2014-06-08 18:21:45 -0500

    Semi-random style tweaks

--- src/radagast/coverage.clj
+++ src/radagast/coverage.clj
@@ -4,9 +4,10 @@
 (def ^{:dynamic true} *ns-must-match* nil)
 
 (defn skip-ns? [n]
-  (or (re-find #"(^clojure\.|radagast)" (str (.getName n)))
+  (let [name (str (.getName n))]
+    (or (re-find #"(^clojure\.|radagast)" name)
       (and *ns-must-match*
-           (not (re-find *ns-must-match* (str (.getName n)))))))
+           (not (re-find *ns-must-match* name)))))
 
 (defn instrument [f v]
   (fn [& args]
@@ -15,20 +16,25 @@
 
 (defn instrument-nses [nses]
   (doseq [n nses [_ v] (ns-publics n)]
-    (when (and (.isBound v) (fn? @v)
-               (not (:macro (meta v))) (not (:test (meta v))))
+    (when (and (.isBound v)
+               (fn? @v)
+               (not (:macro (meta v)))
+               (not (:test (meta v))))
       (alter-meta! v assoc ::untested? true)
       (alter-var-root v instrument v))))
 
 (defn uncovered [nses]
-  (for [n nses [_ v] (ns-publics n) :when (::untested? (meta v))] v))
+  (for [n nses [_ v] (ns-publics n)
+        :when (::untested? (meta v))]
+    v))
 
 (defn -main [whitelist & test-nses]
   (binding [*ns-must-match* (re-pattern whitelist)]
     (doseq [n test-nses]
       (require (symbol n)))
 
-    (let [impl-nses (remove skip-ns? (all-ns))]
+    (let [impl-nses (->> (all-ns)
+                         (remove skip-ns?))]
       (instrument-nses impl-nses)
 
       (with-out-str
