e8acdd6f490875c66a3bf86d81e0eb7668576b7a
Phil Hagelberg <technomancy@gmail.com>
2014-06-11 09:54:48 -0700

    Version 2.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject radagast "2.0.0"
+(defproject radagast "2.0.1-SNAPSHOT"
   :description "Simplistic test coverage."
   :url "https://github.com/technomancy/radagast"
   :license "Eclipse Public License 1.0")
